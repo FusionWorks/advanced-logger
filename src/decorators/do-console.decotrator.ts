8045287b585051130752e1bbb724c1f3963f8178
grigoreme <gmeleca@fusionworks.md>
2019-04-15 12:25:34 +0300

    [travis skip] Execute given code inside final console method.

--- src/decorators/do-console.decotrator.ts
+++ src/decorators/do-console.decotrator.ts
@@ -5,9 +5,11 @@
 // tslint:disable-next-line: function-name
 export function DoFromConsole() {
   return function (target?: any, propertyKey?: string | symbol, descriptor?: PropertyDescriptor) {
+    const original = descriptor.value;
     descriptor.value = function () {
       const args = arguments;
       this.consoleKeeper[propertyKey].apply(this, args);
+      original.apply(this);
     };
     return descriptor;
   };
